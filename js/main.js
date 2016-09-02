070b428b2a9958e804a9c204cad7e3398679d13e
Brian Hurst <brian.hurst@gsa.gov>
2016-09-02 17:29:38 -0400

    Add smooth scroll

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(function() {
       top: $('#pb-nav--side').offset().top
     }
   });
+
+  $('#pb-nav--side a').smoothScroll({
+    afterScroll: function(options) {
+      var $tgt = $(options.scrollTarget);
+      $tgt.attr('tabIndex', '0');
+      // Using $tgt[0] allows us to call .focus() on the DOM node itself, not the jQuery collection
+      $tgt[0].focus();
+    }
+  });
 });
